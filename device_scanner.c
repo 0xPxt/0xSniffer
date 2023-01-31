02d44b58b0879bb1b56f5e08a333c8208ef9aebc
Awwre <24184979+Awwre@users.noreply.github.com>
2023-01-31 23:27:06 +0100

    Initialize library and fix findalldevs

--- device_scanner.c
+++ device_scanner.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
-#include <pcap.h> 
+#include <pcap.h>
+
+#if _WIN32
+#else
 #include <sys/socket.h>
+#endif
 
 #define HAVE_REMOTE
 #define DEVICE_LIST_MAX_SIZE 50
 
+char errbuf[PCAP_ERRBUF_SIZE];
+
 int scan_all_available_devices(char**);
-void display_error(char *error_message, char *errbuf);
+void display_error(char *error_message);
 
 int main (int argc, char** argv) {
     char *devices_list[DEVICE_LIST_MAX_SIZE];
     int num_of_devices = 0;
+    int errNum = 0;
+
+    // Initialize library to use local encoding
+    errNum = pcap_init(PCAP_CHAR_ENC_LOCAL, errbuf);
+    if (errNum == PCAP_ERROR) {
+        display_error("Could not initialize pcap library!");
+    }
 
     num_of_devices = scan_all_available_devices(devices_list);
 
@@ -20,28 +33,29 @@ int main (int argc, char** argv) {
 }
 
 int scan_all_available_devices(char **devices_names) {
-    pcap_if_t **all_devices;
-    char *errbuf;
+    pcap_if_t *all_devices;
+    pcap_if_t *currentDevice;
     int num_of_devices = 0;
 
     //Find list of nd non-loopback devices
-    if(pcap_findalldevs(all_devices, errbuf) == -1) {
-        display_error("Error scanning for devices", errbuf);
+    if(pcap_findalldevs(&all_devices, errbuf) == PCAP_ERROR) {
+        display_error("Error scanning for devices");
     }
 
-    num_of_devices = sizeof(all_devices)/sizeof(all_devices[0]);
-
     //Assing list to argument buffer
-    for(int i = 0; i < num_of_devices; i++) {
-        devices_names[i] = all_devices[i]->name;
+    currentDevice = all_devices;
+    while(currentDevice != NULL) {
+        devices_names[num_of_devices++] = currentDevice->name;
+        currentDevice = currentDevice->next;
     }
 
     return num_of_devices;
 }
 
-void display_error(char *error_message, char *errbuf) {
+void display_error(char *error_message) {
     printf("-=-=-=-=-=-=-= ERROR =-=-=-=-=-=-=-\n");
     printf("%s\n", error_message);
     printf("[ERROR] : %s\n", errbuf);
     printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    exit(1);
 }
