be7c0d26d2a91348165f42a237c36cc122d4a61e
0xPxt <raventosaleix@gmail.com>
2023-01-31 21:27:43 +0100

    cleaner scan and error reporting function

--- device_scanner.c
+++ device_scanner.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
-#define HAVE_REMOTE
 #include <pcap.h> 
 #include <sys/socket.h>
 
+#define HAVE_REMOTE
+#define DEVICE_LIST_MAX_SIZE 50
+
+int scan_all_available_devices(char**);
+void display_error(char *error_message, char *errbuf);
+
 int main (int argc, char** argv) {
+    char *devices_list[DEVICE_LIST_MAX_SIZE];
+    int num_of_devices = 0;
+
+    num_of_devices = scan_all_available_devices(devices_list);
+
+    for(int i = 0; i < num_of_devices; i++) {
+        printf("%s\n", devices_list[i]);
+    }
+}
+
+int scan_all_available_devices(char **devices_names) {
     pcap_if_t **all_devices;
     char *errbuf;
     int num_of_devices = 0;
-    
-    //Add list of found non-loopback devices to all_devices
+
+    //Find list of nd non-loopback devices
     if(pcap_findalldevs(all_devices, errbuf) == -1) {
-        printf("Error scanning for devices\n");
-        printf("Error : %s\n", errbuf);
+        display_error("Error scanning for devices", errbuf);
     }
 
     num_of_devices = sizeof(all_devices)/sizeof(all_devices[0]);
 
-    //Print name to stdout
+    //Assing list to argument buffer
     for(int i = 0; i < num_of_devices; i++) {
-        printf("%s\n", all_devices[i]->name);
+        devices_names[i] = all_devices[i]->name;
     }
 
+    return num_of_devices;
+}
+
+void display_error(char *error_message, char *errbuf) {
+    printf("-=-=-=-=-=-=-= ERROR =-=-=-=-=-=-=-\n");
+    printf("%s\n", error_message);
+    printf("[ERROR] : %s\n", errbuf);
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 }
