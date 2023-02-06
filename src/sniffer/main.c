cc09d6688cfc866a052dd508bf42c14293697a70
Awwre <24184979+Awwre@users.noreply.github.com>
2023-02-06 22:38:19 +0100

    Fix compilation for windows

--- src/sniffer/main.c
+++ src/sniffer/main.c
@@ -19,9 +19,6 @@
 #include <signal.h>
 #endif
 
- void itoa(int n, char s[]);
- void reverse(char s[]);
-
 /* 4 bytes IP address */
 typedef struct ip_address
 {
@@ -230,6 +227,37 @@ static void CreateAndStartLogger() {
     CloseHandle(g_hChildStd_IN_Rd);
 }
 #else
+void itoa(int n, char s[]);
+void reverse(char s[]);
+
+/* reverse:  reverse string s in place */
+void reverse(char s[])
+{
+    int i, j;
+    char c;
+    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+/* itoa:  convert n to characters in s */
+void itoa(int n, char s[])
+{
+    int i, sign;
+    if ((sign = n) < 0)  /* record sign */
+        n = -n;          /* make n positive */
+    i = 0;
+    do {       /* generate digits in reverse order */
+        s[i++] = n % 10 + '0';   /* get next digit */
+    } while ((n /= 10) > 0);     /* delete it */
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+
 void *Sniffer(void *lpParam) {
     pcap_t *handle = (pcap_t *)lpParam;
     pcap_loop(handle, 0, PacketHandler, NULL);
@@ -427,33 +455,3 @@ void PacketHandler(u_char *param, const struct pcap_pkthdr *header, const u_char
 
     WriteToPipe(writeBuffer, WRITE_BUFFER_SIZE);
 }
-
- /* reverse:  reverse string s in place */
- void reverse(char s[])
- {
-     int i, j;
-     char c;
-
-     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
- }
-
- /* itoa:  convert n to characters in s */
- void itoa(int n, char s[])
- {
-     int i, sign;
-
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     reverse(s);
- }
